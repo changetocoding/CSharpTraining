940d986582c22a51ddd1e19cecb3036067d226f8
davmarkman <i.david.mark@yandex.com>
2021-09-06 11:41:29 +0200

    Add unit test class code

---
+++ Lesson3-UnitTesting/Test/Test.Tests/BigramsTests.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Tests
+{
+    [TestFixture]
+    class BigramsTests
+    {
+
+        [Test]
+        public void TestCanCountSingleBigram()
+        {
+            // arrange
+            var bigramCounter = new BigramCounter();
+
+            // act
+            var results = bigramCounter.Count("se");
+
+
+            // assert
+            Assert.That(results, Has.Count.EqualTo(1));
+        }
+
+        [Test]
+        public void WhenMoreThanOneBiagram_AllAreCounted()
+        {
+            // arrange
+            var bigramCounter = new BigramCounter();
+
+            // act
+            var results = bigramCounter.Count("see ");
+
+
+            // assert
+            Assert.That(results, Has.Count.EqualTo(3));
+        }
+
+
+        [Test]
+        public void CanReturnCountOfABigramInString()
+        {
+            // arrange
+            var bigramCounter = new BigramCounter();
+
+            // act
+            var results = bigramCounter.Count("see ");
+
+
+            // assert
+            Assert.That(results["se"], Is.EqualTo(1));
+        }
+
+
+        [Test]
+        public void CanReturnCountOfABigramInString2()
+        {
+            // arrange
+            var bigramCounter = new BigramCounter();
+
+            // act
+            var results = bigramCounter.Count("see ");
+
+
+            // assert
+            Assert.That(results["ee"], Is.EqualTo(1));
+            Assert.That(results.Values, Has.All.EqualTo(1));
+        }
+
+        [Test]
+        public void SpacesAreTreatedAsUnderscore()
+        {
+            // arrange
+            var bigramCounter = new BigramCounter();
+
+            // act
+            var results = bigramCounter.Count("  ");
+
+
+            // assert
+            Assert.That(results, Has.Count.EqualTo(1));
+            Assert.That(results["__"], Is.EqualTo(1));
+        }
+
+        [Test]
+        public void When5Es_TheCountIsEqualTo4()
+        {
+            // arrange
+            var bigramCounter = new BigramCounter();
+
+            // act
+            var result = bigramCounter.Count("eeeee");
+
+            // assert
+            Assert.That(result, Has.Count.EqualTo(1));
+            Assert.That(result["ee"], Is.EqualTo(4));
+
+        }
+    }
+}
---
+++ Lesson3-UnitTesting/Test/Test/BigramCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    public class BigramCounter
+    {
+        public Dictionary<string,int> Count(string input)
+        {
+            input = input.Replace(" ", "_");
+            return Pairs(input)
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, y => y.Count());
+            
+        }
+
+        public IEnumerable<string> Pairs(string input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                yield return $"{input[i]}{input[i + 1]}";
+            }
+        }
+    }
+}
